1859fdc8592458218199f0693a2b77144ffbb479
mdfahim123123 <mdf522201@gmail.com>
2025-08-29 16:38:00 +0600

    my function

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("js file connected")
+const calls = document.getElementsByClassName("call-btn")
+for(let call of calls){
+    console.log(call)
+}
+
+
+
+const coin = document.getElementById('coin-number').innerText
--- tailwaind.config.js
+++
@@ -1,8 +0,0 @@
-const calls = document.getElementsByClassName("call-btn")
-for(const call of calls){
-    console.log(call)
-}
-
-
-
-const coin = document.getElementById('coin-number').innerText
